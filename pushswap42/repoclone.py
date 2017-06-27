d588adc154c11724da28d0306886f7e2a37ccfe5
Aaron Hunt <ach5910@gmail.com>
2017-06-27 04:40:00 +0000

    scripts dir

--- pushswap42/repoclone.py
+++
@@ -1,6 +0,0 @@
-from git import Repo
-
-def clone_repo(url):
-    print("\n\n" + str(url) + "\n\n")
-    Repo.clone_from(url, "../repo/")
-    
--- pushswap42/scripts/repoclone.py
+++ pushswap42/scripts/repoclone.py
@@ -1,6 +1,7 @@
 from git import Repo
+import sys
 
 def clone_repo(url):
     print("\n\n" + str(url) + "\n\n")
-    Repo.clone_from(url, "../../repo/push_swap")
+    Repo.clone_from(url, "../repo/push_swap")
     
